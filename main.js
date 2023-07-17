4495e4d812872638a37a99fe69c8bebc410ec365
Roman <84925444+DoczzzMega@users.noreply.github.com>
2023-07-17 18:37:42 +0500

    created

---
+++ main.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const mediaFiles = document.querySelectorAll('img, video');
+    let i = 0;
+    let currentPercent = 0;
+
+    Array.from(mediaFiles).forEach((file, index) => {
+        file.onload = () => {
+            i++;
+            console.log(index);
+            currentPercent = ((i * 100) / mediaFiles.length).toFixed(1);
+            percents.innerHTML = currentPercent;
+
+            if(i === mediaFiles.length) {
+                preloader.classList.add('preloader--hide')
+                percents.innerHTML = 100;
+            }
+        }
+    })
+
+})
+
+
+
